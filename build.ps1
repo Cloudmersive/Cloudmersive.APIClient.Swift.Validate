3b8f096859943b309f4c144461d0416ccc392003
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-06-29 13:01:25 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -8,19 +8,6 @@
 
 
 
-$csprojpath = Resolve-Path ./client/src/Cloudmersive.APIClient.NET.Validate/Cloudmersive.APIClient.NET.Validate.csproj
-$csprojtestpath = Resolve-Path ./client/src/Cloudmersive.APIClient.NET.Validate.Test/Cloudmersive.APIClient.NET.Validate.Test.csproj
-$nuspecpath = Resolve-Path ./client/src/Cloudmersive.APIClient.NET.Validate/Cloudmersive.APIClient.NET.Validate.nuspec
-$slnpath = Resolve-Path ./client/Cloudmersive.APIClient.NET.Validate.sln
-
-
-(Get-Content $nuspecpath).replace('<title>Swagger Library</title>', "<title>Cloudmersive Validate API Client</title>") | Set-Content $nuspecpath
-(Get-Content $nuspecpath).replace('<authors>$author$</authors>', "<authors>Cloudmersive</authors>") | Set-Content $nuspecpath
-(Get-Content $nuspecpath).replace('<owners>$author$</owners>', "<owners>Cloudmersive</owners>") | Set-Content $nuspecpath
-(Get-Content $nuspecpath).replace('<description>A library generated from a Swagger doc</description>', "<description>The validation APIs help you validate data. Check if an E-mail address is real. Check if a domain is real. Check up on an IP address, and even where it is located. All this and much more is available in the validation API.</description>") | Set-Content $nuspecpath
-(Get-Content $nuspecpath).replace('<!-- Authors contain text that appears directly on the gallery -->', "<iconUrl>https://cloudmersive.com/images/cmsdk.png</iconUrl>") | Set-Content $nuspecpath
-
-
 
 
 
