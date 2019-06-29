1c4b62ccba772593c62fb66c1ef5a2a620f314a6
Cloudmersive <35204726+Cloudmersive@users.noreply.github.com>
2019-06-29 13:05:26 -0700

    Update build.ps1

--- build.ps1
+++ build.ps1
@@ -1,5 +1,5 @@
 ﻿Remove-Item –path ./client –recurse
-& java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/validate -l swift -o client -c packageconfig.json
+& java -jar swagger-codegen-cli-2.4.5.jar generate -i https://api.cloudmersive.com/swagger/api/validate -l swift4 -o client -c packageconfig.json
 
 
 
